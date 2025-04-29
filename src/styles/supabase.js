95a44c557782403bff58cca8afb9f19c61ca61a0
Teds <marktedra29@gmail.com>
2025-04-29 15:14:29 +0800

    do a thing
    install style componenet
    build reusable style component
    setting up page and routes
    build AppLayOut
    SideBar and NavBar
    install supabase
    conntect supabase with ReactApp

---
+++ src/styles/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://qijhdgjvkhctrxyfmyif.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpamhkZ2p2a2hjdHJ4eWZteWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MDAwNDIsImV4cCI6MjA2MTQ3NjA0Mn0.4PdTrpZhlv5tSUmjFwZMDoQBohvwfjlIp4cgfXadT8E";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import {
+  HiOutlineCalendarDateRange,
+  HiOutlineCog8Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
@@ -6,7 +13,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +51,47 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">
+            <HiOutlineCalendarDateRange />
+            <span>Booking</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/users">
+            <HiOutlineUsers />
+            <span>Booking</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/settings">
+            <HiOutlineCog8Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
