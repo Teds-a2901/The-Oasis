da980997fba05553994b360ba6c52e010b182812
Teds <marktedra29@gmail.com>
2025-05-26 10:45:18 +0800

    do a thing
    Verify the authentication session for users
    create Protection  Routes
    create and implement userUser Hooks
    getCurrent user for the async function API Auth

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as LoginAPI } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => LoginAPI({ email, password }),
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
     onError: (err) => {
