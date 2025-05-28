2797bff00ec3167d6c9eb0a9bc283e3f835fc817
Teds <marktedra29@gmail.com>
2025-05-28 14:22:06 +0800

    do a thing
    Fixing a bug from the Menu button not close when its click again and remove the default credential from login

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -104,7 +105,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutSideClcik(close);
+  const ref = useOutSideClcik(close, false);
 
   if (openId !== id) return null;
 
